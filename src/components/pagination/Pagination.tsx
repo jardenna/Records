ea467e6232e9ff148b6fcb760f65d040c27cb49a
jardenna <hbjornum@gmail.com>
2024-11-03 17:44:41 +0100

    pagination

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,19 +1,113 @@
 import { FC } from 'react';
 import './_pagination.scss';
+import PaginationItems from './PaginationItems';
 
 export interface PaginationProps {
   currentPage: number;
-  onSetCurrentPage: (pageNo: number) => void;
-  roundedPaginationCount: number;
-  pageLimit?: number; // Optional prop to specify the number of pages shown at a time
+  handleFirstPage: () => void;
+  handleLastPage: () => void;
+  handleNext: () => void;
+  handlePageClick: (currentPage: number) => void;
+  handlePrevious: () => void;
+  paginationCount: number;
+  startPage: number;
+  pageLimit?: number;
 }
 
-const Pagination: FC<PaginationProps> = () => (
-  <article>
-    <ul className="pagination">
-      <li>test</li>
-    </ul>
-  </article>
-);
+const Pagination: FC<PaginationProps> = ({
+  startPage,
+  handlePrevious,
+  handleFirstPage,
+  handlePageClick,
+  handleLastPage,
+  currentPage,
+  paginationCount,
+  handleNext,
+  pageLimit = 5,
+}) => {
+  const endPage = Math.min(startPage + pageLimit - 1, paginationCount);
+  const pageRange = Array.from(
+    { length: endPage - startPage + 1 },
+    (_, i) => startPage + i,
+  );
+
+  return (
+    <article>
+      <ul className="pagination">
+        {/* Jump Previous */}
+        {startPage > 1 && (
+          <li className="pagination-item">
+            <button type="button" onClick={handlePrevious}>
+              Jump Previous
+            </button>
+          </li>
+        )}
+
+        {/* First Page */}
+        <li className="pagination-item">
+          <button
+            type="button"
+            onClick={handleFirstPage}
+            disabled={currentPage === 1}
+          >
+            First
+          </button>
+        </li>
+
+        {/* Prev Page */}
+        <li className="pagination-item">
+          <button
+            type="button"
+            onClick={() => handlePageClick(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Prev
+          </button>
+        </li>
+
+        {/* Page Numbers */}
+        {pageRange.map((page: any) => (
+          <PaginationItems
+            key={page}
+            onSetCurrentPage={handlePageClick}
+            paginationCount={page}
+            currentPage={currentPage}
+          />
+        ))}
+
+        {/* Next Page */}
+        <li className="pagination-item">
+          <button
+            type="button"
+            onClick={() => handlePageClick(currentPage + 1)}
+            disabled={currentPage === paginationCount}
+          >
+            Next
+          </button>
+        </li>
+
+        {/* Jump Next */}
+        {endPage < paginationCount && (
+          <li className="pagination-item">
+            <button type="button" onClick={handleNext}>
+              Jump Next
+            </button>
+          </li>
+        )}
+
+        {/* Last Page */}
+        <li className="pagination-item">
+          <button
+            type="button"
+            onClick={handleLastPage}
+            disabled={currentPage === paginationCount}
+          >
+            Last
+          </button>
+        </li>
+      </ul>
+    </article>
+  );
+};
 
 export default Pagination;
--- src/pages/Records.tsx
+++ src/pages/Records.tsx
@@ -1,7 +1,6 @@
 import { FC, useState } from 'react';
 import Select from 'react-select';
-import '../components/pagination/_pagination.scss';
-import PaginationItems from '../components/pagination/PaginationItems';
+import Pagination from '../components/pagination/Pagination';
 import { useGetPaginatedRecordsQuery } from '../features/records/recordsApiSlice';
 
 const Records: FC = () => {
@@ -13,6 +12,8 @@ const Records: FC = () => {
     page: currentPage,
     limit: rowsCount,
   });
+
+  console.log(records);
 
   const options = [
     { value: 10, label: '10' },
@@ -60,92 +61,23 @@ const Records: FC = () => {
     setStartPage(paginationCount - pageLimit + 1);
   };
 
-  const endPage = Math.min(startPage + pageLimit - 1, paginationCount);
-  const pageRange = Array.from(
-    { length: endPage - startPage + 1 },
-    (_, i) => startPage + i,
-  );
-
   return (
     <section>
       <h1>Records</h1>
       <div>
         {rowsCount} of {records?.recordsCount} records
       </div>
-      <ul className="pagination">
-        {/* Jump Previous */}
-        {startPage > 1 && (
-          <li className="pagination-item">
-            <button type="button" onClick={handlePrevious}>
-              Jump Previous
-            </button>
-          </li>
-        )}
 
-        {/* First Page */}
-        <li className="pagination-item">
-          <button
-            type="button"
-            onClick={handleFirstPage}
-            disabled={currentPage === 1}
-          >
-            First
-          </button>
-        </li>
-
-        {/* Prev Page */}
-        <li className="pagination-item">
-          <button
-            type="button"
-            onClick={() => handlePageClick(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            Prev
-          </button>
-        </li>
-
-        {/* Page Numbers */}
-        {pageRange.map((page) => (
-          <PaginationItems
-            key={page}
-            onSetCurrentPage={handlePageClick}
-            paginationCount={page}
-            currentPage={currentPage}
-          />
-        ))}
-
-        {/* Next Page */}
-        <li className="pagination-item">
-          <button
-            type="button"
-            onClick={() => handlePageClick(currentPage + 1)}
-            disabled={currentPage === paginationCount}
-          >
-            Next
-          </button>
-        </li>
-
-        {/* Jump Next */}
-        {endPage < paginationCount && (
-          <li className="pagination-item">
-            <button type="button" onClick={handleNext}>
-              Jump Next
-            </button>
-          </li>
-        )}
-
-        {/* Last Page */}
-        <li className="pagination-item">
-          <button
-            type="button"
-            onClick={handleLastPage}
-            disabled={currentPage === paginationCount}
-          >
-            Last
-          </button>
-        </li>
-      </ul>
-
+      <Pagination
+        currentPage={currentPage}
+        handleFirstPage={handleFirstPage}
+        handleLastPage={handleLastPage}
+        handleNext={handleNext}
+        handlePageClick={handlePageClick}
+        handlePrevious={handlePrevious}
+        paginationCount={paginationCount}
+        startPage={startPage}
+      />
       <Select
         options={options}
         onChange={(e: any) => setRowsCount(e.value)}
