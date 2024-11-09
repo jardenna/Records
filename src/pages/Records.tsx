68663c3faa9c6bcb9b9e69e39e5368478cd59f4c
jardenna <hbjornum@gmail.com>
2024-11-09 12:45:51 +0100

    renamed handle functions to on

--- src/pages/Records.tsx
+++ src/pages/Records.tsx
@@ -1,6 +1,5 @@
 import { FC, useState } from 'react';
 import Select from 'react-select';
-import PaginationItems from '../components/pagination/PaginationItems';
 import usePagination from '../components/pagination/usePagination';
 import RecordTable from '../components/recordTable/RecordTable';
 import {
@@ -9,6 +8,7 @@ import {
 } from '../features/records/recordsApiSlice';
 
 import '../components/pagination/_pagination.scss';
+import Pagination from '../components/pagination/Pagination';
 
 const Records: FC = () => {
   const { data: recordCount } = useGetAmountOfRecordsQuery();
@@ -42,85 +42,14 @@ const Records: FC = () => {
       <div>
         {rowsCount * currentPage} of {totalCount} records
       </div>
-      <ul className="pagination">
-        {/* Jump Previous */}
-        {currentPage > 5 && (
-          <li className="pagination-item">
-            <button
-              type="button"
-              onClick={() => handlePaginationAction('jump-prev')}
-            >
-              Jump Previous
-            </button>
-          </li>
-        )}
-
-        {/* First Page */}
-        <li className="pagination-item">
-          <button
-            type="button"
-            onClick={() => handlePaginationAction('first')}
-            disabled={currentPage === 1}
-          >
-            First
-          </button>
-        </li>
-
-        {/* Prev Page */}
-        <li className="pagination-item">
-          <button
-            type="button"
-            onClick={() => handlePaginationAction('prev')}
-            disabled={currentPage === 1}
-          >
-            Prev
-          </button>
-        </li>
-
-        {/* Page Numbers */}
-        {pageRange.map((page: any) => (
-          <PaginationItems
-            key={page}
-            onSetCurrentPage={handlePageClick}
-            paginationCount={page}
-            currentPage={currentPage}
-          />
-        ))}
-
-        {/* Next Page */}
-        <li className="pagination-item">
-          <button
-            type="button"
-            onClick={() => handlePaginationAction('next')}
-            disabled={currentPage === totalPageCount}
-          >
-            Next
-          </button>
-        </li>
-
-        {/* Jump Next */}
-        {currentPage < totalPageCount - 5 && (
-          <li className="pagination-item">
-            <button
-              type="button"
-              onClick={() => handlePaginationAction('jump-next')}
-            >
-              Jump Next
-            </button>
-          </li>
-        )}
-
-        {/* Last Page */}
-        <li className="pagination-item">
-          <button
-            type="button"
-            onClick={() => handlePaginationAction('last')}
-            disabled={currentPage === totalPageCount}
-          >
-            Last
-          </button>
-        </li>
-      </ul>
+      <Pagination
+        currentPage={currentPage}
+        onPageClick={handlePageClick}
+        onPaginationAction={handlePaginationAction}
+        pageLimit={5}
+        pageRange={pageRange}
+        totalPageCount={totalPageCount}
+      />
 
       <Select
         options={[
