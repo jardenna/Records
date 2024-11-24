98fc32c2d1a73b3206474aeea38a2cfc4dd2e9cf
jardenna <hbjornum@gmail.com>
2024-11-24 12:53:46 +0100

    Portal

--- src/pages/toasts/ToastList.tsx
+++ src/pages/toasts/ToastList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Portal from '../../components/Portal';
 import { selectDuration, selectToasts, Toast } from '../../features/toastSlice';
 import ToastComponent from './ToastComponent';
 
@@ -7,16 +8,22 @@ const ToastList: React.FC = () => {
   const toasts = useSelector(selectToasts);
   const autoHideDuration = useSelector(selectDuration);
 
+  if (toasts.length === 0) {
+    return null;
+  }
+
   return (
-    <div className="toast-container">
-      {toasts.map((toast: Toast) => (
-        <ToastComponent
-          key={toast.id}
-          toast={toast}
-          autoHideDuration={autoHideDuration}
-        />
-      ))}
-    </div>
+    <Portal portalId="toast">
+      <div className="toast-container">
+        {toasts.map((toast: Toast) => (
+          <ToastComponent
+            key={toast.id}
+            toast={toast}
+            autoHideDuration={autoHideDuration}
+          />
+        ))}
+      </div>
+    </Portal>
   );
 };
 
