a0ed9ee15bd1b96d2a42f8db1533c94992c9e35c
jardenna <hbjornum@gmail.com>
2024-12-01 09:47:33 +0100

    added year regex clientside

--- src/types/regex.ts
+++ src/types/regex.ts
@@ -1,2 +1,2 @@
-/* eslint-disable import/prefer-default-export */
 export const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+export const yearRegex = /^(18(8[9]|9[0-9])|19\d{2}|20\d{2}|2099)$/;
