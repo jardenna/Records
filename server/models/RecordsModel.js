cc14db8bcd4e520d963731880cd3c0d577b463f2
jardenna <hbjornum@gmail.com>
2024-12-01 09:44:46 +0100

    regex for year and update error response from model

--- server/models/RecordsModel.js
+++ server/models/RecordsModel.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 
-const yearRegex = /^(18|19|20)\d{2}$/;
+const yearRegex = /^(18(8[9]|9[0-9])|19\d{2}|20\d{2}|2099)$/;
+const currentYear = new Date().getFullYear();
+const nextYear = currentYear + 1;
 
 const RecordSchema = mongoose.Schema(
   {
@@ -18,7 +20,8 @@ const RecordSchema = mongoose.Schema(
         validator: function (v) {
           return yearRegex.test(v);
         },
-        message: (props) => `${props.value} is not a valid Year!`,
+        message: (props) =>
+          `${props.value} must be a valid number between 1889 and ${nextYear}`,
       },
       required: [true, 'Please enter a production year'],
     },
@@ -33,7 +36,17 @@ const RecordSchema = mongoose.Schema(
       type: Number,
       default: 1,
     },
-    released: String,
+
+    released: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return yearRegex.test(v);
+        },
+        message: (props) =>
+          `${props.value} must be a valid number between 1889 and ${nextYear}`,
+      },
+    },
     info: String,
     photo: String,
   },
