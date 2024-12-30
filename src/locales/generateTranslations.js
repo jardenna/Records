2e7511a7ddd492d5ffa51474c72c3c64773bfc69
jardenna <hbjornum@gmail.com>
2024-12-30 13:48:10 +0100

    generate language json in locales folder

--- src/locales/generateTranslations.js
+++ src/locales/generateTranslations.js
@@ -7,15 +7,12 @@ const results = [];
 fs.createReadStream('src/locales/translation.csv')
   .pipe(csv({ separator: ';', headers: ['key', 'en', 'da'] })) // Specify separator and headers
   .on('data', (data) => {
-    console.log('Row:', data); // Debugging each row
     results.push(data);
   })
   .on('end', () => {
     try {
       const enJson = {};
       const daJson = {};
-
-      console.log('Parsed Results:', results); // Debug full results
 
       results.forEach((row) => {
         if (row.key && row.en && row.da) {
@@ -26,16 +23,11 @@ fs.createReadStream('src/locales/translation.csv')
         }
       });
 
-      console.log('EN JSON:', enJson);
-      console.log('DA JSON:', daJson);
-
       // Write to en.json
-      fs.writeFileSync('en.json', JSON.stringify(enJson, null, 2));
-      console.log('en.json file written successfully.');
+      fs.writeFileSync('src/locales/en.json', JSON.stringify(enJson, null, 2));
 
       // Write to da.json
-      fs.writeFileSync('da.json', JSON.stringify(daJson, null, 2));
-      console.log('da.json file written successfully.');
+      fs.writeFileSync('src/locales/da.json', JSON.stringify(daJson, null, 2));
     } catch (error) {
       console.error('Error processing CSV:', error);
     }
