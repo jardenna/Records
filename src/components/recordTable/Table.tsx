df54d859aa38ddcd6c0e5216baf704b99ee8b63e
jardenna <hbjornum@gmail.com>
2024-12-31 10:17:59 +0100

    generic types for table

--- src/components/recordTable/Table.tsx
+++ src/components/recordTable/Table.tsx
@@ -1,20 +1,13 @@
 import { Link } from 'react-router';
-import { SortOrder } from '../../app/api/apiTypes';
 import { BtnVariant, MainPath } from '../../types/enums';
 import Button from '../Button';
 import Icon, { IconName } from '../icons/Icon';
 import './_table.scss';
 
-// Define an interface with id
-interface Identifiable {
-  id: string;
-}
-
-// Extend T to ensure it includes `id`
-interface TableProps<T extends Identifiable> {
+interface TableProps<T> {
   caption: string;
   headers: string[];
-  onSort: any;
+  onSort: (field: keyof T) => void;
   searchParams: string;
   sortField: string;
   sortOrder: string;
@@ -22,7 +15,7 @@ interface TableProps<T extends Identifiable> {
   className?: string;
 }
 
-const Table = <T extends Identifiable>({
+const Table = <T extends Record<string, any>>({
   caption,
   headers,
   tableData,
@@ -31,61 +24,52 @@ const Table = <T extends Identifiable>({
   sortField,
   sortOrder,
   className,
-}: TableProps<T>) => {
-  const handleTest = (header: string) => {
-    onSort(header);
-  };
-
-  return (
-    <div className={`table-container ${className}`}>
-      <table>
-        <caption className="visually-hidden">{caption}</caption>
-        <thead>
-          <tr>
-            {headers.map((header) => (
-              <th scope="col" key={header}>
-                <Button
-                  variant={BtnVariant.Ghost}
-                  onClick={() => handleTest(header)}
-                >
-                  {header}
-                  {sortField === header && (
-                    <Icon
-                      size="16"
-                      name={
-                        sortOrder === SortOrder.Desc
-                          ? IconName.ArrowDown
-                          : IconName.ArrowUp
-                      }
-                      title="Sort by"
-                    />
-                  )}
-                </Button>
-              </th>
-            ))}
-            <th className="detail-table-header">Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.map((data: any, rowIndex) => (
-            <tr key={rowIndex}>
-              {headers.map((header) => (
-                <td key={header}>{data[header]}</td>
-              ))}
-              <td className="detail-td">
-                <Link
-                  className="btn btn-primary"
-                  to={`/${MainPath.Details}/${data.id}${searchParams}`}
-                >
-                  Details
-                </Link>
-              </td>
-            </tr>
+}: TableProps<T>) => (
+  <div className={`table-container ${className}`}>
+    <table>
+      <caption className="visually-hidden">{caption}</caption>
+      <thead>
+        <tr>
+          {headers.map((header) => (
+            <th scope="col" key={String(header)}>
+              <Button variant={BtnVariant.Ghost} onClick={() => onSort(header)}>
+                {String(header)}
+                {sortField === header && (
+                  <Icon
+                    size="16"
+                    name={
+                      sortOrder === 'desc'
+                        ? IconName.ArrowDown
+                        : IconName.ArrowUp
+                    }
+                    title="Sort by"
+                  />
+                )}
+              </Button>
+            </th>
           ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+          <th className="detail-table-header">Details</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map((data, rowIndex) => (
+          <tr key={rowIndex}>
+            {headers.map((header) => (
+              <td key={String(header)}>{data[header]}</td>
+            ))}
+            <td className="detail-table-header">
+              <Link
+                className="btn btn-primary"
+                to={`/${MainPath.Details}/${data.id}${searchParams}`}
+              >
+                Details
+              </Link>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
 
 export default Table;
