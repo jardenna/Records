91637543ce85b0828e14c5f82f72bf63c90a2208
jardenna <hbjornum@gmail.com>
2025-01-13 14:13:09 +0100

    styles table

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -26,101 +26,97 @@ const Pagination: FC<PaginationProps> = ({
   const { language } = useLanguage();
 
   return (
-    <article>
-      <ul className="pagination">
-        {/* First Page */}
-        <PaginationItem
-          onSetCurrentPage={() =>
-            onPaginationAction(PaginationActionEnum.First)
-          }
-          disabled={currentPage === 1}
-          content={
-            <IconContent
-              iconName={IconName.ChevronsLeft}
-              title={language.gotoFirstPage}
-            />
-          }
-        />
-
-        {/* Prev Page */}
-        <PaginationItem
-          onSetCurrentPage={() => onPaginationAction(PaginationActionEnum.Prev)}
-          disabled={currentPage === 1}
-          content={
-            <IconContent
-              iconName={IconName.ChevronLeft}
-              title={language.gotoPrevPage}
-            />
-          }
-        />
-
-        {/* Jump Previous */}
-        <PaginationItem
-          disabled={currentPage < pageLimit}
-          onSetCurrentPage={() =>
-            onPaginationAction(PaginationActionEnum.PrevPaginationItem)
-          }
-          content={
-            <IconContent
-              iconName={IconName.More}
-              title={language.jumpPagesBack}
-            />
-          }
-        />
-
-        {/* Page Numbers */}
-        {pageRange.map((page) => (
-          <PaginationItem
-            key={page}
-            onSetCurrentPage={() => onPaginationItemClick(page)}
-            paginationCount={page}
-            ariaLabel={`${language.page} ${page} ${language.of} ${totalPageCount}`}
-            isBtnSelected={page === currentPage}
-            ariaDescribedby="current-status"
-            disabled={page === currentPage}
+    <ul className="pagination">
+      {/* First Page */}
+      <PaginationItem
+        onSetCurrentPage={() => onPaginationAction(PaginationActionEnum.First)}
+        disabled={currentPage === 1}
+        content={
+          <IconContent
+            iconName={IconName.ChevronsLeft}
+            title={language.gotoFirstPage}
           />
-        ))}
+        }
+      />
 
-        {/* Jump Next */}
-        <PaginationItem
-          onSetCurrentPage={() =>
-            onPaginationAction(PaginationActionEnum.NextPaginationItem)
-          }
-          disabled={currentPage > totalPageCount - pageLimit}
-          content={
-            <IconContent
-              iconName={IconName.More}
-              title={language.jumpPagesForth}
-            />
-          }
-        />
+      {/* Prev Page */}
+      <PaginationItem
+        onSetCurrentPage={() => onPaginationAction(PaginationActionEnum.Prev)}
+        disabled={currentPage === 1}
+        content={
+          <IconContent
+            iconName={IconName.ChevronLeft}
+            title={language.gotoPrevPage}
+          />
+        }
+      />
 
-        {/* Next Page */}
-        <PaginationItem
-          onSetCurrentPage={() => onPaginationAction(PaginationActionEnum.Next)}
-          disabled={currentPage === totalPageCount}
-          content={
-            <IconContent
-              iconName={IconName.ChevronRight}
-              title={language.gotoNextPage}
-            />
-          }
-        />
+      {/* Jump Previous */}
+      <PaginationItem
+        disabled={currentPage < pageLimit}
+        onSetCurrentPage={() =>
+          onPaginationAction(PaginationActionEnum.PrevPaginationItem)
+        }
+        content={
+          <IconContent
+            iconName={IconName.More}
+            title={language.jumpPagesBack}
+          />
+        }
+      />
 
-        {/* Last Page */}
+      {/* Page Numbers */}
+      {pageRange.map((page) => (
         <PaginationItem
-          onSetCurrentPage={() => onPaginationAction(PaginationActionEnum.Last)}
-          disabled={currentPage === totalPageCount}
-          className="last-pagination-item"
-          content={
-            <IconContent
-              iconName={IconName.ChevronsRight}
-              title={language.gotoLastPage}
-            />
-          }
+          key={page}
+          onSetCurrentPage={() => onPaginationItemClick(page)}
+          paginationCount={page}
+          ariaLabel={`${language.page} ${page} ${language.of} ${totalPageCount}`}
+          isBtnSelected={page === currentPage}
+          ariaDescribedby="current-status"
+          disabled={page === currentPage}
         />
-      </ul>
-    </article>
+      ))}
+
+      {/* Jump Next */}
+      <PaginationItem
+        onSetCurrentPage={() =>
+          onPaginationAction(PaginationActionEnum.NextPaginationItem)
+        }
+        disabled={currentPage > totalPageCount - pageLimit}
+        content={
+          <IconContent
+            iconName={IconName.More}
+            title={language.jumpPagesForth}
+          />
+        }
+      />
+
+      {/* Next Page */}
+      <PaginationItem
+        onSetCurrentPage={() => onPaginationAction(PaginationActionEnum.Next)}
+        disabled={currentPage === totalPageCount}
+        content={
+          <IconContent
+            iconName={IconName.ChevronRight}
+            title={language.gotoNextPage}
+          />
+        }
+      />
+
+      {/* Last Page */}
+      <PaginationItem
+        onSetCurrentPage={() => onPaginationAction(PaginationActionEnum.Last)}
+        disabled={currentPage === totalPageCount}
+        className="last-pagination-item"
+        content={
+          <IconContent
+            iconName={IconName.ChevronsRight}
+            title={language.gotoLastPage}
+          />
+        }
+      />
+    </ul>
   );
 };
 
