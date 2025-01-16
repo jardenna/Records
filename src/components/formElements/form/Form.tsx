bb4846d3eff8c8a8078e66c5fbde20929958150c
jardenna <hbjornum@gmail.com>
2025-01-16 16:12:01 +0100

    cancel btn

--- src/components/formElements/form/Form.tsx
+++ src/components/formElements/form/Form.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from 'react';
 import useLanguage from '../../../features/language/useLanguage';
 import LayoutElement from '../../../layout/LayoutElement';
+import { BtnVariant } from '../../../types/enums';
 import { FormEventType } from '../../../types/types';
 import Button from '../../Button';
 import './_form.scss';
@@ -23,12 +24,19 @@ const Form: FC<FormProps> = ({
   ariaLabel,
 }) => {
   const { language } = useLanguage();
-  console.log(ariaLabel);
 
   return (
     <form onSubmit={onSubmit} noValidate className={className}>
       {children}
       <LayoutElement className="form-footer" ariaLabel={language.form}>
+        <Button
+          type="submit"
+          isLoading={isLoading}
+          ariaLabel={ariaLabel}
+          variant={BtnVariant.Secondary}
+        >
+          {language.cancel}
+        </Button>
         <Button type="submit" isLoading={isLoading} ariaLabel={ariaLabel}>
           {labelText}
         </Button>
