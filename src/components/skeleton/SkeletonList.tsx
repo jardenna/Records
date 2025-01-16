b27f9d85d77d836d6c3830b8e1a5db2b8c9a66b0
jardenna <hbjornum@gmail.com>
2025-01-16 09:05:12 +0100

    skeletonlist

--- src/components/skeleton/SkeletonList.tsx
+++ src/components/skeleton/SkeletonList.tsx
@@ -2,17 +2,28 @@ import { FC } from 'react';
 import Skeleton from './Skeleton';
 
 export interface SkeletonListProps {
-  height: number;
-  width: number;
+  className?: string;
   count?: number;
+  height?: number;
+  width?: number;
 }
 
-const SkeletonList: FC<SkeletonListProps> = ({ count = 2, width, height }) => {
+const SkeletonList: FC<SkeletonListProps> = ({
+  count = 2,
+  width,
+  height,
+  className = '',
+}) => {
   const skeletons = Array.from({ length: count });
   return (
     <span className="skeleton-list">
       {skeletons.map((_, index) => (
-        <Skeleton key={index} height={height} width={width} />
+        <Skeleton
+          key={index}
+          height={height}
+          width={width}
+          className={className}
+        />
       ))}
     </span>
   );
