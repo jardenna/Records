c0b18d938f250824bd05438bb1c52fe0fdb11c63
jardenna <hbjornum@gmail.com>
2025-01-21 08:11:06 +0100

    skeleton grid

--- src/components/createOrUpdateForm/CreateOrUpdateForm.tsx
+++ src/components/createOrUpdateForm/CreateOrUpdateForm.tsx
@@ -13,7 +13,7 @@ import validateUpdate, {
   minimumYear,
 } from '../formElements/validation/validateUpdate';
 import RecordImg from '../shared/recordImg/RecordImg';
-import Skeleton from '../skeleton/Skeleton';
+import SkeletonGrid from '../skeleton/SkeletonGrid';
 import './_create-update-form.scss';
 
 //  onUpdateRecord?: (values: Records) => void;
@@ -82,139 +82,141 @@ const CreateOrUpdateForm: FC<CreateOrUpdateFormProps> = ({
       />
 
       <section>
-        <Form
-          onSubmit={onSubmit}
-          onCancel={() => navigate(-1)}
-          labelText={title}
-          ariaLabel={isLoading ? 'Loading' : undefined}
-          isLoading={isLoading}
-          className="create-update-form"
-        >
-          <div className="grid three-col">
-            <fieldset className="flex column">
-              <VisuallyHidden as="legend">{language.albumInfo}</VisuallyHidden>
-              <Input
-                name="artist"
-                id="artist"
-                onChange={onChange}
-                onBlur={onBlur}
-                errorText={errors.artist}
-                value={values.artist}
-                labelText={language.artist}
-                required
-              />
-              <Input
-                name="title"
-                id="title"
-                onChange={onChange}
-                onBlur={onBlur}
-                errorText={
-                  errors.title && `${language.pleaseEnter} ${language.title}`
-                }
-                value={values.title}
-                labelText={language.title}
-                required
-              />
-              <Input
-                name="prodYear"
-                id="prodYear"
-                onChange={onChange}
-                onBlur={onBlur}
-                errorText={errors.prodYear}
-                type="number"
-                min={minimumYear}
-                max={maxYear}
-                value={values.prodYear || ''}
-                labelText={language.prodYear}
-                maxLength={4}
-                required
-              />
-              <Input
-                name="released"
-                id="released"
-                onChange={onChange}
-                value={values.released || ''}
-                labelText={language.released}
-                min={Number(values.prodYear) + 1 && minimumYear}
-                max={maxYear}
-                type="number"
-                maxLength={4}
-                errorText={errors.released}
-                onBlur={onBlur}
-              />
-              <Input
-                name="label"
-                id="label"
-                onChange={onChange}
-                value={values.label}
-                labelText={language.label}
-              />
-              <Input
-                name="recordNo"
-                id="recordNo"
-                onChange={onChange}
-                value={values.recordNo}
-                labelText={language.recordNo}
-              />
-            </fieldset>
+        {isLoading ? (
+          <SkeletonGrid />
+        ) : (
+          <Form
+            onSubmit={onSubmit}
+            onCancel={() => navigate(-1)}
+            labelText={title}
+            ariaLabel={isLoading ? 'Loading' : undefined}
+            isLoading={isLoading}
+            className="create-update-form"
+          >
+            <div className="grid three-col">
+              <fieldset className="flex column">
+                <VisuallyHidden as="legend">
+                  {language.albumInfo}
+                </VisuallyHidden>
+                <Input
+                  name="artist"
+                  id="artist"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  errorText={errors.artist}
+                  value={values.artist}
+                  labelText={language.artist}
+                  required
+                />
+                <Input
+                  name="title"
+                  id="title"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  errorText={
+                    errors.title && `${language.pleaseEnter} ${language.title}`
+                  }
+                  value={values.title}
+                  labelText={language.title}
+                  required
+                />
+                <Input
+                  name="prodYear"
+                  id="prodYear"
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  errorText={errors.prodYear}
+                  type="number"
+                  min={minimumYear}
+                  max={maxYear}
+                  value={values.prodYear || ''}
+                  labelText={language.prodYear}
+                  maxLength={4}
+                  required
+                />
+                <Input
+                  name="released"
+                  id="released"
+                  onChange={onChange}
+                  value={values.released || ''}
+                  labelText={language.released}
+                  min={Number(values.prodYear) + 1 && minimumYear}
+                  max={maxYear}
+                  type="number"
+                  maxLength={4}
+                  errorText={errors.released}
+                  onBlur={onBlur}
+                />
+                <Input
+                  name="label"
+                  id="label"
+                  onChange={onChange}
+                  value={values.label}
+                  labelText={language.label}
+                />
+                <Input
+                  name="recordNo"
+                  id="recordNo"
+                  onChange={onChange}
+                  value={values.recordNo}
+                  labelText={language.recordNo}
+                />
+              </fieldset>
 
-            <fieldset className="flex column">
-              <VisuallyHidden as="legend">
-                {language.additionalInfo}
-              </VisuallyHidden>
-              <Input
-                name="numOfRecords"
-                id="numOfRecords"
-                onChange={onChange}
-                labelText={language.numOfRecords}
-                type="number"
-                value={values.numOfRecords || ''}
-              />
-              <Input
-                name="price"
-                id="price"
-                onChange={onChange}
-                labelText={language.price}
-                value={values.price}
-              />
-              <Textarea
-                name="origin"
-                id="origin"
-                value={values.origin}
-                onChange={onChange}
-                labelText={language.origin}
-              />
-              <Textarea
-                name="info"
-                id="info"
-                value={values.info}
-                onChange={onChange}
-                labelText={language.niceToKnow}
-              />
-            </fieldset>
+              <fieldset className="flex column">
+                <VisuallyHidden as="legend">
+                  {language.additionalInfo}
+                </VisuallyHidden>
+                <Input
+                  name="numOfRecords"
+                  id="numOfRecords"
+                  onChange={onChange}
+                  labelText={language.numOfRecords}
+                  type="number"
+                  value={values.numOfRecords || ''}
+                />
+                <Input
+                  name="price"
+                  id="price"
+                  onChange={onChange}
+                  labelText={language.price}
+                  value={values.price}
+                />
+                <Textarea
+                  name="origin"
+                  id="origin"
+                  value={values.origin}
+                  onChange={onChange}
+                  labelText={language.origin}
+                />
+                <Textarea
+                  name="info"
+                  id="info"
+                  value={values.info}
+                  onChange={onChange}
+                  labelText={language.niceToKnow}
+                />
+              </fieldset>
 
-            <fieldset className="flex column">
-              <VisuallyHidden as="legend">{language.cover}</VisuallyHidden>
-              <FileInput
-                onChange={onChange}
-                name="cover"
-                id="cover"
-                labelText={language.upLoadCover}
-                value=""
-              />
-              {!isLoading ? (
+              <fieldset className="flex column">
+                <VisuallyHidden as="legend">{language.cover}</VisuallyHidden>
+                <FileInput
+                  onChange={onChange}
+                  name="cover"
+                  id="cover"
+                  labelText={language.upLoadCover}
+                  value=""
+                />
                 <RecordImg
                   src={recordDetails?.cover || 'default.png'}
                   previewUrl={imgUpdated ? previewUrl : null}
                   title={file?.name || 'Preview'}
                   alt=""
                 />
-              ) : (
-                <Skeleton variant="img" />
-              )}
-            </fieldset>
-          </div>
-        </Form>
+              </fieldset>
+            </div>
+          </Form>
+        )}
       </section>
     </>
   );
