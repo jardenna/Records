d977bb2a00e55352923eff3cb84cee1c68fb52f4
jardenna <hbjornum@gmail.com>
2025-02-05 07:39:36 +0100

    auth form

--- src/features/auth/components/AuthForm.tsx
+++ src/features/auth/components/AuthForm.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
+import Button from '../../../components/Button';
 import VisuallyHidden from '../../../components/VisuallyHidden';
 import Figure from '../../../components/figure/Figure';
 import Input from '../../../components/formElements/Input';
 import Form from '../../../components/formElements/form/Form';
+import { BtnVariant } from '../../../types/enums';
 import { ChangeInputType, FormEventType } from '../../../types/types';
 import useLanguage from '../../language/useLanguage';
 import './_auth-form.scss';
@@ -35,54 +37,63 @@ const AuthForm: FC<AuthFormProps> = ({
 
   return (
     <section className="auth">
-      <Figure src="/images/login_img.jpg" alt="" />
-
-      <Form
-        labelText={labelText}
-        onSubmit={onSubmit}
-        isLoading={isLoading}
-        className="auth-form"
-      >
-        <fieldset className="flex column">
-          <VisuallyHidden as="legend">{legendText}</VisuallyHidden>
-          {values.username !== undefined && (
+      <Figure src="/images/login_img.jpg" alt={language.authImgAlt} />
+      <div className="auth-container">
+        <ul className="auth-menu">
+          <li>
+            <Button variant={BtnVariant.Ghost}>Login</Button>
+          </li>
+          <li>
+            <Button variant={BtnVariant.Ghost}>Register</Button>
+          </li>
+        </ul>
+        <Form
+          labelText={labelText}
+          onSubmit={onSubmit}
+          isLoading={isLoading}
+          className="auth-form"
+        >
+          <fieldset className="flex column">
+            <VisuallyHidden as="legend">{legendText}</VisuallyHidden>
+            {values.username !== undefined && (
+              <Input
+                name="username"
+                id="username"
+                value={values.username}
+                labelText={language.username}
+                onChange={onChange}
+                required
+              />
+            )}
             <Input
-              name="username"
-              id="username"
-              value={values.username}
-              labelText={language.username}
+              name="email"
+              id="email"
+              value={values.email}
+              labelText={language.email}
               onChange={onChange}
               required
             />
-          )}
-          <Input
-            name="email"
-            id="email"
-            value={values.email}
-            labelText={language.email}
-            onChange={onChange}
-            required
-          />
-          <Input
-            name="password"
-            id="password"
-            value={values.password}
-            labelText={language.password}
-            onChange={onChange}
-            required
-          />
-          {values.confirmPassword !== undefined && (
             <Input
-              name="confirmPassword"
-              id="confirmPassword"
-              value={values.confirmPassword}
-              labelText={language.confirmPassword}
+              name="password"
+              id="password"
+              value={values.password}
+              labelText={language.password}
               onChange={onChange}
               required
             />
-          )}
-        </fieldset>
-      </Form>
+            {values.confirmPassword !== undefined && (
+              <Input
+                name="confirmPassword"
+                id="confirmPassword"
+                value={values.confirmPassword}
+                labelText={language.confirmPassword}
+                onChange={onChange}
+                required
+              />
+            )}
+          </fieldset>
+        </Form>
+      </div>
     </section>
   );
 };
