88d1c3939f05b17fe3813b907214a55ea674daf4
jardenna <hbjornum@gmail.com>
2025-02-06 10:41:00 +0100

    protected route removed useEffect for navigate

--- src/features/auth/pages/ProtectedRoute.tsx
+++ src/features/auth/pages/ProtectedRoute.tsx
@@ -1,19 +1,12 @@
-import React, { useEffect } from 'react';
-import { Navigate, Outlet, useNavigate } from 'react-router';
+import React from 'react';
+import { Navigate, Outlet, useLocation } from 'react-router';
 import Skeleton from '../../../components/skeleton/Skeleton';
 import { MainPath } from '../../../types/enums';
 import { useCheckAuthQuery } from '../authApiSlice';
 
 const ProtectedRoute: React.FC = () => {
-  const navigate = useNavigate();
-  const { data: userProfile, isLoading, isError } = useCheckAuthQuery();
-
-  useEffect(() => {
-    if (!isLoading && userProfile?.success === false) {
-      // Redirect to login if user is not authenticated
-      navigate('/login');
-    }
-  }, [isLoading, isError]);
+  const location = useLocation();
+  const { data: userProfile, isLoading } = useCheckAuthQuery();
 
   if (isLoading) {
     return (
@@ -23,10 +16,10 @@ const ProtectedRoute: React.FC = () => {
     );
   }
 
-  return userProfile && userProfile.success === true ? (
+  return userProfile ? (
     <Outlet />
   ) : (
-    <Navigate to={MainPath.Login} />
+    <Navigate to={MainPath.Login} state={{ from: location }} replace />
   );
 };
 
