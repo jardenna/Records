d00190dc3507261f184b06a68b75fd6e32ebad5c
jardenna <hbjornum@gmail.com>
2025-02-09 11:43:44 +0100

    deleted skeleton from protectedroute

--- src/features/auth/pages/ProtectedRoute.tsx
+++ src/features/auth/pages/ProtectedRoute.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router';
-import Skeleton from '../../../components/skeleton/Skeleton';
 import { MainPath } from '../../../layout/nav/enums';
 import useAuth from '../hooks/useAuth';
 
 const ProtectedRoute: React.FC = () => {
   const location = useLocation();
-  const { currentUser, isLoading } = useAuth();
-
-  if (isLoading || currentUser === undefined) {
-    return (
-      <div>
-        <Skeleton count={4} />
-      </div>
-    );
-  }
+  const { currentUser } = useAuth();
 
   if (!currentUser) {
     return <Navigate to={MainPath.Login} state={{ from: location }} replace />;
