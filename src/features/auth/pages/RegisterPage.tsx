323f37d25c5aaff6ea15123b7f970342c8778a01
jardenna <hbjornum@gmail.com>
2025-02-10 14:40:59 +0100

    Registerpage

--- src/features/auth/pages/RegisterPage.tsx
+++ src/features/auth/pages/RegisterPage.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react';
+import { useLocation, useNavigate } from 'react-router';
+import useMessagePopup from '../../../components/messagePopup/useMessagePopup';
 import useFormValidation from '../../../hooks/useFormValidation';
+import { MainPath } from '../../../layout/nav/enums';
 import useLanguage from '../../language/useLanguage';
 import { useRegisterMutation } from '../authApiSlice';
 import AuthForm from '../components/AuthForm';
 
 const RegisterPage: FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const { language } = useLanguage();
   const initialState = {
     username: '',
@@ -13,17 +18,41 @@ const RegisterPage: FC = () => {
     confirmPassword: '',
   };
 
+  const { addMessagePopup } = useMessagePopup();
+  const from = location.state?.from?.pathname || MainPath.Root;
+
   const { values, onChange, onSubmit } = useFormValidation({
     initialState,
     callback: handleRegisterUser,
   });
   const [registerUser, { isLoading }] = useRegisterMutation();
 
-  function handleRegisterUser() {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { confirmPassword, ...rest } = values;
+  async function handleRegisterUser() {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { confirmPassword, ...rest } = values;
 
-    registerUser(rest);
+      const result = await registerUser(rest).unwrap();
+      if (result.success === true) {
+        navigate(from, { replace: true });
+      }
+
+      if (result.success === false) {
+        addMessagePopup({
+          message: result.message,
+          messagePopupType: 'error',
+          componentType: 'notification',
+          position: 'top-center',
+        });
+      }
+    } catch (error: any) {
+      addMessagePopup({
+        messagePopupType: 'error',
+        message: error.data.message,
+        componentType: 'notification',
+        position: 'top-center',
+      });
+    }
   }
 
   return (
