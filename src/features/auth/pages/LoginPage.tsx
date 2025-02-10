a0d87eea7f0cc2892b8887e49bf84604f7397b64
jardenna <hbjornum@gmail.com>
2025-02-10 14:09:50 +0100

    fix refresh

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 import useMessagePopup from '../../../components/messagePopup/useMessagePopup';
 import useFormValidation from '../../../hooks/useFormValidation';
@@ -6,13 +6,12 @@ import { MainPath } from '../../../layout/nav/enums';
 import useLanguage from '../../language/useLanguage';
 import { useLoginMutation } from '../authApiSlice';
 import AuthForm from '../components/AuthForm';
-import useAuth from '../hooks/useAuth';
 
 const LoginPage: FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { language } = useLanguage();
-  const { currentUser, isLoading: isAuthLoading } = useAuth(); // Get current user from auth state
+
   const initialState = {
     email: '',
     password: '',
@@ -25,12 +24,15 @@ const LoginPage: FC = () => {
     initialState,
     callback: handleLoginUser,
   });
-  const [loginUser, { isLoading: isLoginLoading, isSuccess }] =
-    useLoginMutation();
+  const [loginUser, { isLoading: isLoginLoading }] = useLoginMutation();
 
   async function handleLoginUser() {
     try {
       const result = await loginUser(values).unwrap();
+
+      if (result.success === true) {
+        navigate(from, { replace: true });
+      }
 
       if (result.success === false) {
         addMessagePopup({
@@ -50,18 +52,12 @@ const LoginPage: FC = () => {
     }
   }
 
-  useEffect(() => {
-    if (isSuccess && currentUser) {
-      navigate(from, { replace: true });
-    }
-  }, [isSuccess, currentUser, navigate, from]);
-
   return (
     <AuthForm
       values={values}
       labelText={language.login}
       onSubmit={onSubmit}
-      isLoading={isLoginLoading || isAuthLoading}
+      isLoading={isLoginLoading}
       legendText={language.userInfo}
       onChange={onChange}
     />
