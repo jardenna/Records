f5dcabccee533f42e2a29758cade4a725022de20
jardenna <hbjornum@gmail.com>
2025-02-12 12:55:36 +0100

    valid password

--- src/components/formElements/password/PasswordPopupList.tsx
+++ src/components/formElements/password/PasswordPopupList.tsx
@@ -1,28 +1,26 @@
 import { FC } from 'react';
-import PasswordPopupItem from './PasswordPopupItem';
+import { isNumber, isSymbol, lowercase, uppercase } from '../../../utils/regex';
 
 interface PasswordPopupListProps {
   inputValue: string;
 }
 
 const PasswordPopupList: FC<PasswordPopupListProps> = ({ inputValue }) => {
-  const passwordRequirements = [
-    'Mindst 8 tegn',
-    'Mindst ét tal',
-    'Mindst ét lille bogstav',
-    'Mindst ét stort bogstav',
-    'Mindst ét specielt tegn',
+  const passwordRules = [
+    { text: 'Mindst 8 tegn', isValid: inputValue.length >= 8 },
+    { text: 'Mindst ét tal', isValid: isNumber.test(inputValue) },
+    { text: 'Mindst ét lille bogstav', isValid: lowercase.test(inputValue) },
+    { text: 'Mindst ét stort bogstav', isValid: uppercase.test(inputValue) },
+    { text: 'Mindst ét specielt tegn', isValid: isSymbol.test(inputValue) },
   ];
 
   return (
     <article>
       <ul className="popup-item-list">
-        {passwordRequirements.map((requirement) => (
-          <PasswordPopupItem
-            text={requirement}
-            key={requirement}
-            inputValue={inputValue}
-          />
+        {passwordRules.map(({ text, isValid }, index) => (
+          <li key={index} className={`popup-item ${!isValid ? 'error' : ''}`}>
+            {text}
+          </li>
         ))}
       </ul>
     </article>
