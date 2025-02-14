9d369db76f8bf3e8966aaae6bac79126b5e45b27
jardenna <hbjornum@gmail.com>
2025-02-14 15:12:57 +0100

    new table

---
+++ src/components/table/MainTable.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import { Records } from '../../app/api/apiTypes';
+import useLanguage from '../../features/language/useLanguage';
+import './_table.scss';
+
+interface MainTableProps {
+  data: Records[];
+  tableHeaders: string[];
+}
+
+const MainTable: FC<MainTableProps> = ({ tableHeaders, data }) => {
+  const { language } = useLanguage();
+
+  return (
+    <div className="tbl-container">
+      <div className="row tbl-fixed">
+        <table className="main-table">
+          <thead>
+            <tr>
+              {tableHeaders.map((header) => (
+                <th key={header} scope="col">
+                  {language[header]}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((album) => (
+              <tr>
+                <td>{album.artist}</td>
+                <td>{album.title}</td>
+                <td>{album.prodYear}</td>
+                <td>{album.label}</td>
+                <td>{album.origin}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default MainTable;
