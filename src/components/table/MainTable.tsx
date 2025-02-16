72fd12be0c4eb5c832c494b6dae4a48a63bec6d6
jardenna <hbjornum@gmail.com>
2025-02-16 10:45:48 +0100

    Table comp

--- src/components/table/MainTable.tsx
+++ src/components/table/MainTable.tsx
@@ -1,6 +1,5 @@
 import { FC, useState } from 'react';
 import { Records } from '../../app/api/apiTypes';
-import useLanguage from '../../features/language/useLanguage';
 import { MainPath } from '../../layout/nav/enums';
 import { ChangeInputType } from '../../types/types';
 import { IconName } from '../icons/Icon';
@@ -8,8 +7,7 @@ import ActionBody from '../recordTable/ActionBody';
 import RecordTableHeader from '../recordTable/RecordTableHeader';
 import ActionHeader from '../recordTable/TableActionHeader';
 import TableGridIcons from '../recordTable/TableGridIcons';
-import VisuallyHidden from '../VisuallyHidden';
-import './_table.scss';
+import Table from './Table';
 
 interface MainTableProps {
   id: string | null;
@@ -42,8 +40,6 @@ const MainTable: FC<MainTableProps> = ({
   values,
   onSort,
 }) => {
-  const { language } = useLanguage();
-
   const [padding, setPadding] = useState(12);
 
   const handlePadding = (variant: number) => {
@@ -68,53 +64,44 @@ const MainTable: FC<MainTableProps> = ({
         isActive={padding}
       />
 
-      <div className="fixed-table">
-        <table
-          className="main-table"
-          aria-label={isLoading ? language.loading : undefined}
-        >
-          <VisuallyHidden as="caption">{tableCaption}</VisuallyHidden>
-
-          <thead>
-            <tr>
-              {tableHeaders.map((header) => (
-                <th scope="col" style={style} key={header}>
-                  <RecordTableHeader
-                    onSort={onSort}
-                    showIcon={valuesFromSearch.sortField === header}
-                    sortOrder={sortOrder}
-                    title={header}
-                    onFilterRows={onFilterRows}
-                    value={
-                      (valuesFromSearch[header] as string) || values[header]
-                    }
-                  />
-                </th>
-              ))}
-              <ActionHeader onClearAllSearch={onClearAllSearch} />
-            </tr>
-          </thead>
-          <tbody>
-            {tableData.map((album) => (
-              <tr key={album.id}>
-                <td style={style}>{album.artist}</td>
-                <td>{album.title}</td>
-                <td>{album.prodYear}</td>
-                <td>{album.label}</td>
-                <td>{album.origin}</td>
-                <ActionBody
-                  onViewAlbum={() => onViewAlbum(album.id)}
-                  modalId={id === album.id ? album.id : null}
-                  id={id}
-                  name={album.artist}
-                  to={`/${MainPath.Update}/${album.id}`}
-                  onOpenModal={() => onOpenModal(album.id)}
+      <Table isLoading={isLoading} tableCaption={tableCaption}>
+        <thead>
+          <tr>
+            {tableHeaders.map((header) => (
+              <th scope="col" style={style} key={header}>
+                <RecordTableHeader
+                  onSort={onSort}
+                  showIcon={valuesFromSearch.sortField === header}
+                  sortOrder={sortOrder}
+                  title={header}
+                  onFilterRows={onFilterRows}
+                  value={(valuesFromSearch[header] as string) || values[header]}
                 />
-              </tr>
+              </th>
             ))}
-          </tbody>
-        </table>
-      </div>
+            <ActionHeader onClearAllSearch={onClearAllSearch} />
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((album) => (
+            <tr key={album.id}>
+              <td style={style}>{album.artist}</td>
+              <td>{album.title}</td>
+              <td>{album.prodYear}</td>
+              <td>{album.label}</td>
+              <td>{album.origin}</td>
+              <ActionBody
+                onViewAlbum={() => onViewAlbum(album.id)}
+                modalId={id === album.id ? album.id : null}
+                id={id}
+                name={album.artist}
+                to={`/${MainPath.Update}/${album.id}`}
+                onOpenModal={() => onOpenModal(album.id)}
+              />
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </>
   );
 };
