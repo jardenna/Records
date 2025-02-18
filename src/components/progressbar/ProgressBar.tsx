80291c285312eac5120c136d014e59303efef0db
jardenna <hbjornum@gmail.com>
2025-02-18 07:44:50 +0100

    updates to progressbar

--- src/components/progressbar/ProgressBar.tsx
+++ src/components/progressbar/ProgressBar.tsx
@@ -1,26 +1,27 @@
 import { FC } from 'react';
+import useLanguage from '../../features/language/useLanguage';
 import './_progress-bar.scss';
 
 interface ProgressBarProps {
   progressPercentage: number;
-  variant?: 'primary' | 'secondary';
+  isAuto?: boolean;
 }
 
-const ProgressBar: FC<ProgressBarProps> = ({
-  progressPercentage,
-  variant = 'primary',
-}) => (
-  <div
-    className="progress-bar-container"
-    role="progressbar"
-    aria-label="progress bar"
-  >
+const ProgressBar: FC<ProgressBarProps> = ({ progressPercentage, isAuto }) => {
+  const { language } = useLanguage();
+  return (
     <span
-      style={{ width: `${progressPercentage}%` }}
-      className={`progress-bar  ${variant}`}
-      aria-valuetext={`Progress ${progressPercentage}% completed`}
-    />
-  </div>
-);
+      className="progress-bar-container"
+      role="progressbar"
+      aria-label={language.progressbar}
+    >
+      <span
+        style={{ width: `${progressPercentage}%` }}
+        className={`progress-bar  ${isAuto ? 'auto-animation' : ''}`}
+        aria-valuetext={`${language.progress} ${progressPercentage}% ${language.completed}`}
+      />
+    </span>
+  );
+};
 
 export default ProgressBar;
