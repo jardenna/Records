1f588a99d861f2754eeb1754bd07b50ec8841898
jardenna <hbjornum@gmail.com>
2025-02-19 15:31:06 +0100

    images only error in upload images

--- server/routes/RecordRoutes.js
+++ server/routes/RecordRoutes.js
@@ -16,19 +16,33 @@ import Record from '../models/RecordModel.js';
 // Get all records
 router.get('/', paginatedResults(Record), getPaginatedRecords);
 
-//Get the Latest 6 records
+// Get the Latest 6 records
 router.get('/latestSix', getLatestSixRecords);
 
 //Get record by Id
 router.get('/:recordId', getRecordById);
 
-//post cover
-router.post('/:recordId', upload.single('cover'), postCreateOrUpdateRecord);
+// Update record
+router.post('/:recordId', (req, res, next) => {
+  upload.single('cover')(req, res, (err) => {
+    if (err) {
+      return res.status(400).json({ message: err.message });
+    }
+    postCreateOrUpdateRecord(req, res, next);
+  });
+});
 
-//Create record
-router.post('/', upload.single('cover'), postCreateOrUpdateRecord);
+// Create record
+router.post('/', (req, res, next) => {
+  upload.single('cover')(req, res, (err) => {
+    if (err) {
+      return res.status(400).json({ message: err.message });
+    }
+    postCreateOrUpdateRecord(req, res, next);
+  });
+});
 
-//Delete Record
+// Delete record
 router.delete('/delete/:recordId', languageMiddleware, deleteRecord);
 
 export default router;
--- server/utils/uploadImages.js
+++ server/utils/uploadImages.js
@@ -39,7 +39,7 @@ function checkFileType(file, cb) {
   if (mimetype && extname) {
     return cb(null, true);
   } else {
-    cb('Error: Images Only!');
+    return cb(new Error('Images Only!'));
   }
 }
 
