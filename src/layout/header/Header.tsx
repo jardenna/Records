274920f1d740e5c8c86ef36edf50d061d6bc5abf
jardenna <hbjornum@gmail.com>
2025-03-01 12:56:53 +0100

    language in header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { Link } from 'react-router';
 import SelectBox from '../../components/selectBox/SelectBox';
+import { SelectedLanguage } from '../../features/language/languageSlice';
+import useLanguage from '../../features/language/useLanguage';
 import LayoutElement from '../LayoutElement';
 import { MainPath } from '../nav/enums';
 import Nav from '../nav/Nav';
@@ -9,34 +11,43 @@ import './_header.scss';
 interface HeaderProps {
   ariaLabel: string;
 }
-const Header: FC<HeaderProps> = ({ ariaLabel }) => (
-  <LayoutElement as="header" className="main-header" ariaLabel={ariaLabel}>
-    <section className="hero">
-      <div className="container">
-        <div className="logo">
-          <Link to={MainPath.Root}>
-            <span>Plade</span>
-            <span className="logo-item">samling</span>
-          </Link>
-        </div>
-        <form>
-          <SelectBox
-            name="limit"
-            options={[
-              { value: 'da', label: 'DK' },
-              { value: 'en', label: 'UK' },
-            ]}
-            id="limit"
-            onChange={() => console.log(12)}
-            labelText="language"
-            inputHasNoLabel
-            defaultValue={{ value: 'da', label: 'DK' }}
-          />
-        </form>
-      </div>
-    </section>
-    <Nav />
-  </LayoutElement>
-);
+const Header: FC<HeaderProps> = ({ ariaLabel }) => {
+  const { switchLanguage, selectedLanguage } = useLanguage();
 
+  const handleLanguageChange = (selectedLanguage: SelectedLanguage) => {
+    switchLanguage(selectedLanguage);
+  };
+
+  return (
+    <LayoutElement as="header" className="main-header" ariaLabel={ariaLabel}>
+      <section className="hero">
+        <div className="container">
+          <div className="logo">
+            <Link to={MainPath.Root}>
+              <span>Plade</span>
+              <span className="logo-item">samling</span>
+            </Link>
+          </div>
+          <form>
+            <SelectBox
+              name="language"
+              options={[
+                { value: 'da', label: 'DK' },
+                { value: 'en', label: 'UK' },
+              ]}
+              id="language"
+              onChange={(selectedOptions: any) =>
+                handleLanguageChange(selectedOptions?.value)
+              }
+              labelText="selectLanguage"
+              inputHasNoLabel
+              defaultValue={{ value: selectedLanguage, label: 'DK' }}
+            />
+          </form>
+        </div>
+      </section>
+      <Nav />
+    </LayoutElement>
+  );
+};
 export default Header;
