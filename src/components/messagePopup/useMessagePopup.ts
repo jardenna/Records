493387960337213eeec5e318a2f2ccff805f1a29
jardenna <hbjornum@gmail.com>
2025-10-14 08:45:52 +0200

    removed line

--- src/components/messagePopup/useMessagePopup.ts
+++ src/components/messagePopup/useMessagePopup.ts
@@ -19,7 +19,6 @@ const useMessagePopup = (messagePopupId?: string) => {
   const handleAddMessagePopup = ({
     message,
     messagePopupType = 'success',
-
     componentType = 'toast',
   }: MessagePopupWithoutId) => {
     dispatch(
